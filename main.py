1777b788e30b7b160ea31c5706850165743c8b79
mustafa3012 <125359501+mus2123@users.noreply.github.com>
2023-03-31 16:53:13 +0000

    Final Code

--- main.py
+++ main.py
@@ -2,36 +2,94 @@
 import streamlit as st
 import sqlite3 as sqlite
 import pandas as pd
+# import plotly.express as px 
 
-## Instructions 
-#streamlit run main.py
+## Instructions:
+# Go in Shell (on the right)
+# write "pip install --upgrade streamlit"
+# launch with "streamlit run main.py"
+
 @st.cache_data
+# Fetching data function
 def get_data():
-  
-  con = sqlite.connect("Fake_sales_data.db")
-  data = pd.read_sql_query("SELECT * from SalesA",con)
-  return data
-  
-data = get_data()  
-
-st.title('sales Dashboard')
-
-st.write(f"we have{len(data)} datapoints")
-
-choice = st.selectbox("select a company",
-data["company"].unique(),index=0)
+    con = sqlite.connect("Fake_sales_data.db")
+    data = pd.read_sql_query("SELECT * from  SalesA", con)
+    con.close() # close the database connection
+    return data
 
 
-new_data = data[data["company"] == choice]
+# Fetching data
+data = get_data()
+data['company'] = data['company'].astype('string')
+data['cat'] = data['cat'].astype('string')
 
-st.write(new_data.head())
-st.write(f"sum of sales{choice}:")
-sperweek = new_data.groupby("week")["price"].sum()
+st.title('Sales Dashboard')
+st.write(f"We have {len(data)} datapoints")
 
-st.write(sperweek)
-st.write(new_data["price"].sum())
-st.line_chart(sperweek, y="price")
+# Company selection
+company = st.selectbox("Select a Company", data["company"].unique(), index=0)
 
-options =st.multiselect()
+# Category selection
+category = st.multiselect("Select a Category", data["cat"].unique(), default="Young")
+
+# Data filtering
+data_selection = data.query("company == @company & cat == @category")
+
+# 1st Chart for Products Sold
+st.write(f"Products Sold for {company} of {category} category:")
+
+st.write(f"We have {len(data_selection)} datapoints")
+
+no_prod_sold = data_selection[['cat', 'price']].value_counts().reset_index()
+no_prod_sold.columns = [*no_prod_sold.columns[:-1], 'count']
+
+st.bar_chart(no_prod_sold,x='price',y='count')
+# plot_bar = px.bar(no_prod_sold,
+#                   x='price',
+#                   y='count',
+#                   color='cat',
+#                   title='<b>Number of Units Sold for Different Products</b>',
+#                   labels={
+#                       "price": "Product Price",
+#                       "count": "Number of Units Sold"
+#                   })
+# st.plotly_chart(plot_bar)
+
+# ----------------------------------------------------------------------------------------
+st.write(f"Volume of Sales for {category} :")
+volume = data_selection["price"].count()
+st.write(volume)
+
+# ----------------------------------------------------------------------------------------
+#revenue
+st.write(f"Revenue for {category} :")
+data_selection['price'] = data['price'].astype('float')
+revenue = data_selection["price"].sum()
+st.write(revenue)
+
+# Volume of sales
+volume_per_week = data_selection.groupby(["week"])["price"].count().reset_index()
+
+# The revenues
+sum_per_week = data_selection.groupby(["week"])["price"].sum().reset_index()
+
+# ----------------------------------------------------------------------------------------
+# plot volume & revenue through time
+tab1, tab2 = st.tabs(["Volume", "Revenue"])
+
+with tab1:
+    figvol = st.line_chart(volume_per_week,
+                   x="week",
+                   y="price")
+    # st.plotly_chart(figvol)
+
+with tab2:
+    figrev = st.line_chart(sum_per_week,
+                   x="week",
+                   y="price")
+    # st.plotly_chart(figrev)
+
+
+
 
 
